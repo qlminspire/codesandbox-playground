f0f838843e3755b37a62165325e904605b76ead4
Uladzislau Karpovich <vladislavqlm@gmail.com>
2022-10-30 09:32:02 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello 123</h1>
+      <h1>Hello 1231</h1>
       <h2>CodeSandbox & Github integration</h2>
     </div>
   );
